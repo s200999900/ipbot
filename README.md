650e17a253d66dcebae30e2e7e38476840e800c0
s200999900 <200999900s@gmail.com>
2014-10-04 19:52:20 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
 ipbot
 =====
 
-Jabber бот для отправки внешнего IP адреса.
-
-Авторство программы принадлежит владельцу сайта: http://devhead.ru
-Оригинал программы: 
-http://devhead.ru/read/jabber-bot-dlya-otpravki-vneshnego-ip-adresa
--- ipbot.py
+++ ipbot.py
@@ -1,58 +1,64 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-'''
-ipbot- IP address to jabber bot.
-licence GPL v.3
-'''
+__author__ = 'svelic'
 
-import  xmpp
-import urllib2
 import ConfigParser
+import urllib2
+import smtplib
+from email.MIMEText import MIMEText
+
+def main():
+    config = ConfigParser.ConfigParser()
+    config.read('ipbot.conf')
+
+    ########Считываем параметры#################
+    email= (config.get('account', 'email'))
+    password=(config.get('account', 'password'))
+    smtp_server= (config.get('account', 'smtp_server'))
+    smtp_port= (config.get('account', 'smtp_port'))
+    ############################################
+
+    print ("Runned with next parameters: ")
+    print ("Email:\t\t\t%s" % email )
+    print ("Password:\t\t%s" % password )
+    print ("Smtp server:\t" + smtp_server + ":" + smtp_port)
+    print ("-" * 40)
+
+    ################Получаем IP##################
+    strURL='http://api.wipmania.com/'
+    f = urllib2.urlopen(urllib2.Request(strURL))
+    response = f.read()
+    ipget= response.split("<br>")
+    f.close()
+    #############################################
+
+    print ("Our external ip: " + ipget[1] + " => " + ipget[0])
+
+    #########Отправка email-а####################
+    # текст письма
+    text = """
+    Привет!
+    Мой текущий IP: %s
+    """ % ipget[1]
+    # заголовок письма
+    subj = 'My external IP'
+
+    # формирование сообщения
+    msg = MIMEText(text, "", "utf-8")
+    msg['Subject'] = subj
+    msg['From'] = email
+    msg['To'] = email
+
+    # отправка
+    s = smtplib.SMTP(smtp_server, smtp_port)
+    s.ehlo()
+    s.starttls()
+    s.ehlo()
+    s.login(email, password)
+    s.sendmail(email, email, msg.as_string())
+    s.quit()
 
 
-config = ConfigParser.ConfigParser()
-config.read('ipbot.conf')
-
-##########################
-user= (config.get('account', 'login'))
-password=(config.get('account', 'password'))
-presence=(config.get('presence','presence'))
-##########################
-
-jid=xmpp.protocol.JID(user)
-client=xmpp.Client(jid.getDomain())
-client.connect()
-client.auth(jid.getNode(),password)
-
-
-
-################Получаем IP##################
-strURL='http://api.wipmania.com/'
-f = urllib2.urlopen(urllib2.Request(strURL))
-response = f.read()
-ipget= response.split("")
-f.close()
-#############################################
-
-
-def status(xstatus):
-    status=xmpp.Presence(status=xstatus,show=presence,priority='1')
-    client.send(msging)
-
-def message(conn,mess):
-
-  global client
-
-
-  if ( mess.getBody() == "ip" ):
-
-  client.send(xmpp.Message(mess.getFrom(),ipget[1]+" => "+ipget[0]))#Отсылаем IP
-
-client.RegisterHandler('message',message)
-
-client.sendInitPresence()
-
-while True:
-
-    client.Process(1)
+if __name__ == "__main__":
+    main()
