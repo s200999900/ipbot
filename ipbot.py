1470429be86c8f5800f9aa900671715029b64f88
s200999900 <200999900s@gmail.com>
2014-10-04 21:46:22 +0300

    initial commit

--- ipbot.py
+++ ipbot.py
@@ -3,20 +3,43 @@
 
 __author__ = 'svelic'
 
+import os
 import ConfigParser
 import urllib2
 import smtplib
 from email.MIMEText import MIMEText
 
+
 def main():
+
+    configfile = 'ipbot.conf'
     config = ConfigParser.ConfigParser()
-    config.read('ipbot.conf')
+
+    if os.path.isfile(configfile) :
+        config.read(configfile)
+    else:
+        config.add_section('account')
+        config.set('account', 'email', 'email@server')
+        config.set('account', 'password', 'PaSSword')
+        config.set('account', 'smtp_server', 'smtp.gmail.com')
+        config.set('account', 'smtp_port', '587')
+        with open(configfile, 'w') as configfile:    # save config
+            config.write(configfile)
+
+        print("Please edit ipbot.conf file! ")
+        return 0
 
     ########Считываем параметры#################
     email= (config.get('account', 'email'))
     password=(config.get('account', 'password'))
     smtp_server= (config.get('account', 'smtp_server'))
     smtp_port= (config.get('account', 'smtp_port'))
+    try:
+        old_ip= (config.get('ip', 'ip'))
+    except ConfigParser.NoSectionError:
+        # Create non-existent section
+        config.add_section('ip')
+        old_ip = ''
     ############################################
 
     print ("Runned with next parameters: ")
@@ -29,26 +52,47 @@ def main():
     strURL='http://api.wipmania.com/'
     f = urllib2.urlopen(urllib2.Request(strURL))
     response = f.read()
-    ipget= response.split("<br>")
+    new_ip= response.split("<br>")
+    new_ip = new_ip[0]
     f.close()
+    if old_ip == new_ip:
+        return 0
+
+    try:
+        config.set('ip', 'ip', new_ip)
+    except ConfigParser.NoSectionError:
+        # Create non-existent section
+        config.add_section('ip')
+    else:
+        with open(configfile, 'w') as configfile:    # save config
+            config.write(configfile)
     #############################################
 
-    print ("Our external ip: " + ipget[1] + " => " + ipget[0])
+    print ("Our external ip: " + new_ip)
 
     #########Отправка email-а####################
     # текст письма
     text = """
     Привет!
     Мой текущий IP: %s
-    """ % ipget[1]
+
+    -------------
+    Br, IpBot
+    """ % new_ip
     # заголовок письма
     subj = 'My external IP'
 
     # формирование сообщения
-    msg = MIMEText(text, "", "utf-8")
+    msg = MIMEText(text, "plain", "utf-8")
     msg['Subject'] = subj
     msg['From'] = email
     msg['To'] = email
+
+    print("Email sending: ")
+    print("From:\t\t" + msg['From'])
+    print("To:\t\t" + msg['To'] )
+    print("Subject:\t" + msg['Subject'])
+    print("Text: \n " + text)
 
     # отправка
     s = smtplib.SMTP(smtp_server, smtp_port)
